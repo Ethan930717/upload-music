bbfb05f8359763fdc47212864c2e6c49f2081406
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-28 17:30:34 +0800

    1

--- src/controllers/UploadController.js
+++ src/controllers/UploadController.js
@@ -11,15 +11,16 @@ import { Folder, __dirname, formatTime } from '../utils.js';
 
 class UploadController {
     index(ctx){
-        const file = ctx.request.files.musicfile
+        const file = ctx.request.files.musicfile;
         try {
             const dirName = formatTime(new Date(), "YYYY-MM-DD");
-            const ext = path.extname(file.originalFilename)
-            const dir = path.join(__dirname, `../public/uploads/${dirName}`);
-            // 检查文件夹是否存在如果不存在则新建文件夹
-            Folder(dir)
-            const fileName = `${new Date().getTime()}${ext}`
-            const saveFilePath = `${dir}/${fileName}`
+            const ext = path.extname(file.originalFilename);
+            // 设置新的文件保存目录
+            const dir = `/home/file.kimoji.club/data/music/${dirName}`;
+            // 检查文件夹是否存在，如果不存在则新建文件夹
+            Folder(dir);
+            const fileName = `${new Date().getTime()}${ext}`;
+            const saveFilePath = `${dir}/${fileName}`;
             // 创建可读流
             const reader = fs.createReadStream(file['filepath']);
             // 创建可写流
@@ -30,14 +31,14 @@ class UploadController {
             ctx.body = {
                 code: 1,
                 message: 'File uploaded successfully.',
-                url: `/uploads/${dirName}/${fileName}` // 返回文件的URL
+                url: `/music/${dirName}/${fileName}` // 根据需要更新返回的文件URL
             };
         } catch (err) {
-            console.log('err: ', );
+            console.log('err: ', err);
             ctx.status = 500;
             ctx.body = {
                 code: 0,
-                message: 'File uploaded error.' + err.toString(),
+                message: 'File upload error.' + err.toString(),
             };
         }
     }
